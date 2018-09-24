8958db5f885d6cf4f1f9b929785b79d41f6c122d
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-09-24 20:24:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,8 +3,13 @@ class Song
   attr_accessor :title, :artist
 
   def initialize(name, artist)
-    @artist = Artist.name(artist)
-    @song = @artist.song(name)
+    @artist = Artist.new(artist)
+    @song = song
   end
+
+  def name
+    
+
+    
 
 end
